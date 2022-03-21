cb0ecdefba486098c3a7bd37195c4d4342f4153f
chunyewang <chunyewang@tencent.com>
2022-03-21 15:05:41 +0800

    add lc001

--- solution/lc070_climbing-stairs_test.go
+++ solution/lc070_climbing-stairs_test.go
@@ -8,14 +8,8 @@ import (
 
 func Test_climbStairs(t *testing.T) {
 	assert.Equal(t, 8, climbStairs(5))
-}
-
-func Test_climbStairsDP(t *testing.T) {
-	assert.Equal(t, 8, climbStairs(5))
-}
-
-func Test_climbStairsDPSimplify(t *testing.T) {
-	assert.Equal(t, 8, climbStairs(5))
+	assert.Equal(t, 8, climbStairsDP(5))
+	assert.Equal(t, 8, climbStairsDPSimplify(5))
 }
 
 func Benchmark_climbStairs(b *testing.B) {
