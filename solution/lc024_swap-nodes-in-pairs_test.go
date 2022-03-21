a02e0fe3455a4b3a7914bb1e5793b6795f8d4d75
chunyewang <chunyewang@tencent.com>
2022-03-21 23:22:12 +0800

    add lc025

--- solution/lc024_swap-nodes-in-pairs_test.go
+++ solution/lc024_swap-nodes-in-pairs_test.go
@@ -5,5 +5,5 @@ import (
 )
 
 func Test_swapPairs(t *testing.T) {
-	
+
 }
--- solution/lc025_reverse-nodes-in-k-group.go
+++ solution/lc025_reverse-nodes-in-k-group.go
@@ -8,7 +8,44 @@ package solution
  * }
  */
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	var prev *ListNode = nil
+	var hair = &ListNode{Next: head}
+	var groupPrev = hair
+	var groupHead = head
+	for groupHead != nil {
+		// 找到kgroup的头部和尾部，如果不够直接返回
+		var groupTail = groupPrev
+		for i:=0; i<k; i++ {
+			groupTail = groupTail.Next
+			if groupTail == nil{
+				return hair.Next
+			}
+		}
 
-	return prev
+		// 记录group的next
+		groupNext := groupTail.Next
+		// 组内反转
+		newGroupHead, newGroupTail := reverseInGroup(groupHead, groupTail)
+
+		// 新的head和tail指针链接
+		groupPrev.Next = newGroupHead
+		newGroupTail.Next = groupNext
+
+		// 跟新groupprev和grouphead，继续下一个循环
+		groupPrev = newGroupTail
+		groupHead = groupNext
+	}
+	return hair.Next
+}
+
+func reverseInGroup(head, tail *ListNode) (*ListNode, *ListNode) {
+	prev := tail.Next
+	curr := head
+	for prev != tail {
+		next := curr.Next
+
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return tail, head
 }
